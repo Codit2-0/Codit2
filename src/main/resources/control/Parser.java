a48c1fd5c2740ae9fee1f9cbf56c3c0906a3435b
silviocresci <silviocresci@192.168.0.57>
2019-10-08 19:37:49 +0200

    Modifiche al Parser e aggiunta dei Bean

--- src/main/resources/control/Parser.java
+++ src/main/resources/control/Parser.java
@@ -6,21 +6,34 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
+
+import model.AssociationBean;
+import model.ERBean;
+import model.EntityBean;
+
 import java.lang.String;
+import java.util.ArrayList;
  
 public class Parser {
  
     final static String ATTRIBUTES = "ownedAttribute";
     final static String CLASSES = "packagedElement"; 
-    final static String FILE_ADDRESS = "/Users/antoniocimino/git/Codit2.0/src/main/test.xmi";
+    final static String FILE_ADDRESS = "/Users/silviocresci/eclipse-workspace/Nattimo/src/test.xmi";
  
     public static void main(String args[]) {
-        try {
+    	 ERBean modello = new ERBean();
+    	 ArrayList<EntityBean> arrayEntity = new ArrayList<EntityBean>();
+    	 ArrayList<AssociationBean> arrayAssociation = new ArrayList<AssociationBean>();
+    	try {
  
             DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
             Document doc = docBuilder.parse(new File(FILE_ADDRESS));
- 
+            ArrayList<String> attribute = null;
+           
+            EntityBean entity = null;
+            AssociationBean association = null;
+            
             doc.getDocumentElement().normalize();
             System.out.println("Root element of the doc is " + doc.getDocumentElement().getNodeName());
  
@@ -32,9 +45,28 @@ public class Parser {
             	
                 String type =  listClass.item(i).getAttributes().getNamedItem("xmi:type").toString();
             	if(type.equals("xmi:type=\"uml:Association\"")) {
-            		System.out.println("Association : " + listClass.item(i).getAttributes().getNamedItem("xmi:id"));
+     
+            		String nomeAssociazione = listClass.item(i).getAttributes().getNamedItem("xmi:id").toString();
+            		nomeAssociazione = nomeAssociazione.substring(nomeAssociazione.indexOf('"')+1, nomeAssociazione.lastIndexOf('"'));
+            		String nomeEntity1 = nomeAssociazione.substring(0, nomeAssociazione.indexOf("__"));
+            		EntityBean e1 = modello.findByName(nomeEntity1);
+            		String nuovaStringa = nomeAssociazione.substring(nomeAssociazione.indexOf("__")+2);
+            		String nomeEntity2 = nuovaStringa.substring(0, nuovaStringa.indexOf("__"));
+            		EntityBean e2 = modello.findByName(nomeEntity2);
+            		attribute = new ArrayList<String>();
+            		association = new AssociationBean(attribute, nomeAssociazione, e1 , e2);
+            		arrayAssociation.add(association);
+            		
             	} else {
-            		System.out.println("Class : " + listClass.item(i).getAttributes().getNamedItem("name"));
+         
+            		String nomeClasse = listClass.item(i).getAttributes().getNamedItem("xmi:id").toString();
+            		nomeClasse = nomeClasse.substring(nomeClasse.indexOf('"')+1, nomeClasse.lastIndexOf('"'));
+            		System.out.println("Classe : "+nomeClasse);
+            		attribute = new ArrayList<String>();
+            		attribute = new ArrayList<String>();
+            		entity = new EntityBean(nomeClasse, attribute);
+            		arrayEntity.add(entity);
+            		
             	}
         
             	NodeList listAttributesClass = listClass.item(i).getChildNodes();
@@ -43,11 +75,21 @@ public class Parser {
             	for(int j = 0; j < totalAttributesClass; j++) {
             		String Attribute = listAttributesClass.item(j).getNodeName();
             		if(Attribute.equals(ATTRIBUTES)) {
-                        System.out.println("Attribute : " + listAttributesClass.item(j).getAttributes().getNamedItem("name"));
+                        String nomeAttributo = listAttributesClass.item(j).getAttributes().getNamedItem("name").toString();
+                        nomeAttributo = nomeAttributo.substring(nomeAttributo.indexOf('"')+1, nomeAttributo.lastIndexOf('"'));
+                        System.out.println("Attribute : "+nomeAttributo);
+                        entity.getAttribute().add(nomeAttributo);
+                        for (int o = 0; o < arrayEntity.size(); o++) {
+                        	if(arrayEntity.get(o).getName().equals(entity.getName())) {
+                        		arrayEntity.remove(o);
+                        		arrayEntity.add(entity);
+                        		break;
+                        }
+                      }
+
             		}
             	}
             	
-            	System.out.println("");
             }
  
         } catch (SAXParseException err) {
@@ -59,5 +101,10 @@ public class Parser {
         } catch (Throwable t) {
             t.printStackTrace();
         }
+    	
+    	
+        modello.setEntity(arrayEntity);
+        modello.setAssociation(arrayAssociation);
+        modello.stampa();
     }
 }
