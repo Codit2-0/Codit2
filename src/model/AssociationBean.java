d771c8df460e01b7e75fe87d4d8f2e40cd8b58c5
FedericoRipoli <32262911+FedericoRipoli@users.noreply.github.com>
2019-10-23 16:14:05 +0200

    Change project
    In questo commit è stato modificato il progetto per problemi di utilizzo di un progetto Maven (se qualcuno sa come funziona, buon per lui). Il file pom.xml è stato inserito anche qui per nella speranza che si riesca a far funzionare il progetto con il continous integration fornito da travis(nel quale abbiamo usato il checkstyle controllato da Maven, se qualcuno riesce a modificarlo ci farebbe un piacere).
    Possiamo considerare questo come commit iniziale e di configurazione (i precedenti possono essere ignorati)

---
+++ src/model/AssociationBean.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.ArrayList;
+
+public class AssociationBean {
+
+  private ArrayList<String> attribute;
+  private String name;
+  private EntityBean entity1;
+  private EntityBean entity2;
+
+  public AssociationBean(ArrayList<String> attribute, String name,
+                EntityBean entity1, EntityBean entity2) {
+    super();
+    this.attribute = attribute;
+    this.name = name;
+    this.entity1 = entity1;
+    this.entity2 = entity2;
+  }
+
+  public ArrayList<String> getAttribute() {
+    return attribute;
+  }
+  
+  public void setAttribute(ArrayList<String> attribute) {
+    this.attribute = attribute;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public EntityBean getEntity1() {
+    return entity1;
+  }
+  
+  public void setEntity1(EntityBean entity1) {
+    this.entity1 = entity1;
+  }
+  
+  public EntityBean getEntity2() {
+    return entity2;
+  }
+  
+  public void setEntity2(EntityBean entity2) {
+    this.entity2 = entity2;
+  }
+
+
+
+}
---
+++ src/model/ERBean.java
@@ -0,0 +1,66 @@
+package model;
+
+import java.util.ArrayList;
+
+public class ERBean {
+
+  private ArrayList<EntityBean> entity;
+  private ArrayList<AssociationBean> association;
+
+  public ERBean() {
+
+  }
+
+  public ERBean(ArrayList<EntityBean> entity, ArrayList<AssociationBean> association) {
+    super();
+    this.entity = entity;
+    this.association = association;
+  }
+
+  public ArrayList<EntityBean> getEntity() {
+    return entity;
+  }
+  
+  public void setEntity(ArrayList<EntityBean> entity) {
+    this.entity = entity;
+  }
+  
+  public ArrayList<AssociationBean> getAssociation() {
+    return association;
+  }
+  
+  public void setAssociation(ArrayList<AssociationBean> association) {
+    this.association = association;
+  }
+
+  public EntityBean findByName(String name) {
+    if(entity!=null)
+      for(int i = 0; i < entity.size(); i++) {
+        if(entity.get(i).getName().equals(name))
+          return entity.get(i);
+      }
+    return null;
+  }
+
+  public AssociationBean findAssociationByName(String name) {
+    if(association!=null)
+      for(int i = 0; i < association.size(); i++) {
+        if(association.get(i).getName().equals(name))
+          return association.get(i);
+      }
+    return null;
+  }
+
+  public void stampa() {
+    if(entity!= null)
+      for(int i = 0; i<entity.size(); i++) {
+        System.out.println(entity.get(i).getName()+"@@@");
+        for (int j = 0; j<entity.get(i).getAttribute().size(); j++) {
+          System.out.println(entity.get(i).getAttribute().get(j));
+        }
+      }
+    if(association!=null)
+      for(int i = 0; i<association.size(); i++)
+        System.out.println(association.get(i).getName()+"@@");
+  }
+}
