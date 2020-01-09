007626f99470328915dc14121e899a838d5d69a5
FedericoRipoli <32262911+FedericoRipoli@users.noreply.github.com>
2020-01-09 16:16:40 +0100

    filtro modal

--- src/main/model/JSONSchemaER.java
+++ src/main/model/JSONSchemaER.java
@@ -15,7 +15,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
 import org.json.simple.JSONObject;
@@ -33,6 +32,7 @@ public class JSONSchemaER extends HttpServlet {
   }
 
 
+  @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
 
@@ -92,6 +92,7 @@ public class JSONSchemaER extends HttpServlet {
 */
   }
 
+  @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     // TODO Auto-generated method stub
