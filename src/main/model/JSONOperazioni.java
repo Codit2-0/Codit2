3686d3cb37ea71fbabef50242b23f27e59169bc7
SportsBar <antoniocimino@antonios-mbp-4.homenet.telecomitalia.it>
2020-01-11 12:28:59 +0100

    Selezione Operandi 3

--- src/main/model/JSONOperazioni.java
+++ src/main/model/JSONOperazioni.java
@@ -23,24 +23,30 @@ public class JSONOperazioni extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String p = (String) request.getParameter("op");
-	
+		
 	    HttpSession sessione = request.getSession();
 		ArrayList operazioniSelezionate = (ArrayList) sessione.getAttribute("operazioniSelezionate");
+		ArrayList latoOperazioni = (ArrayList) sessione.getAttribute("latoOperazioni");
 		if(operazioniSelezionate == null) operazioniSelezionate = new ArrayList();
-		operazioniSelezionate.add(p);
-		sessione.setAttribute("operazioniSelezionate", operazioniSelezionate);
+		if(latoOperazioni == null) latoOperazioni = new ArrayList();
+		
+		if(p != "null" && p != null) {
+			operazioniSelezionate.add(p);
+			latoOperazioni.add("null");
+			sessione.setAttribute("operazioniSelezionate", operazioniSelezionate);
+			sessione.setAttribute("latoOperazioni", latoOperazioni);
+		}
 		
 		response.setContentType("text/html");
 		response.getWriter().append("[");
-		
 		for(int i = 0;i<operazioniSelezionate.size();i++) {
-			if(i != operazioniSelezionate.size()-1) { response.getWriter().append("{\"op\":\"" + operazioniSelezionate.get(i) + "\"},");}
-			else {response.getWriter().append("{\"op\": \"" + operazioniSelezionate.get(i) + "\"}");}
+			if(i != operazioniSelezionate.size() - 1) { response.getWriter().append("{\"op\":\"" + operazioniSelezionate.get(i) + "\", \"scelta\": \""+latoOperazioni.get(i)+"\"},");}
+			else {response.getWriter().append("{\"op\": \"" + operazioniSelezionate.get(i) + "\", \"scelta\": \""+latoOperazioni.get(i)+"\"}");}
 			//System.out.println(operazioniSelezionate.get(i));
 		}
 	    response.getWriter().append("]");
 		
-	   
+	    //sessione.invalidate();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ src/main/model/SceltaOperazione.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SceltaOperazione
+ */
+@WebServlet("/SceltaOperazione")
+public class SceltaOperazione extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SceltaOperazione() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String p = (String) request.getParameter("op");
+		String s = (String) request.getParameter("sc");
+		
+	    HttpSession sessione = request.getSession();
+		ArrayList latoOperazione  = (ArrayList) sessione.getAttribute("latoOperazioni");
+		latoOperazione.set(Integer.parseInt(p),s);
+		sessione.setAttribute("latoOperazione", latoOperazione);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
