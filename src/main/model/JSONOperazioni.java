d881c96e3e72ba0a87ba077caccd5c814de62380
SportsBar <antoniocimino@antonios-mbp-4.homenet.telecomitalia.it>
2020-01-11 14:48:41 +0100

    Aggiunta colori

--- src/main/model/JSONOperazioni.java
+++ src/main/model/JSONOperazioni.java
@@ -3,6 +3,7 @@ package model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -27,21 +28,30 @@ public class JSONOperazioni extends HttpServlet {
 	    HttpSession sessione = request.getSession();
 		ArrayList operazioniSelezionate = (ArrayList) sessione.getAttribute("operazioniSelezionate");
 		ArrayList latoOperazioni = (ArrayList) sessione.getAttribute("latoOperazioni");
+		ArrayList colori = (ArrayList) sessione.getAttribute("colori");
 		if(operazioniSelezionate == null) operazioniSelezionate = new ArrayList();
 		if(latoOperazioni == null) latoOperazioni = new ArrayList();
+		if(colori == null) colori = new ArrayList();
 		
 		if(p != "null" && p != null) {
+			Random random = new Random();
+		    int val = random.nextInt(15728638) + 1048576;
+		    String hex = new String();
+		    hex = Integer.toHexString(val);
+			
 			operazioniSelezionate.add(p);
 			latoOperazioni.add("null");
+			colori.add(hex);
 			sessione.setAttribute("operazioniSelezionate", operazioniSelezionate);
 			sessione.setAttribute("latoOperazioni", latoOperazioni);
+			sessione.setAttribute("colori", colori);
 		}
 		
 		response.setContentType("text/html");
 		response.getWriter().append("[");
 		for(int i = 0;i<operazioniSelezionate.size();i++) {
-			if(i != operazioniSelezionate.size() - 1) { response.getWriter().append("{\"op\":\"" + operazioniSelezionate.get(i) + "\", \"scelta\": \""+latoOperazioni.get(i)+"\"},");}
-			else {response.getWriter().append("{\"op\": \"" + operazioniSelezionate.get(i) + "\", \"scelta\": \""+latoOperazioni.get(i)+"\"}");}
+			if(i != operazioniSelezionate.size() - 1) { response.getWriter().append("{\"op\":\"" + operazioniSelezionate.get(i) + "\", \"scelta\": \""+latoOperazioni.get(i)+"\", \"colore\": \""+colori.get(i)+"\"},");}
+			else {response.getWriter().append("{\"op\": \"" + operazioniSelezionate.get(i) + "\", \"scelta\": \""+latoOperazioni.get(i)+"\", \"colore\": \""+colori.get(i)+"\"}");}
 			//System.out.println(operazioniSelezionate.get(i));
 		}
 	    response.getWriter().append("]");
