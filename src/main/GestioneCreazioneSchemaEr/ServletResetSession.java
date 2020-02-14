061ed58c7191a173eab6860bb8fefddf36b6580f
FedericoRipoli <32262911+FedericoRipoli@users.noreply.github.com>
2020-02-14 18:09:24 +0100

    Correzione errori causati dal revert del merge
    Mea culpa. ho aggiustato

--- src/main/GestioneCreazioneSchemaEr/ServletResetSession.java
+++ src/main/GestioneCreazioneSchemaEr/ServletResetSession.java
@@ -34,7 +34,58 @@ public class ServletResetSession extends HttpServlet {
                       throws ServletException, IOException {
 
     HttpSession sessione = request.getSession();
+    ArrayList operazioniSelezionate = (ArrayList) sessione.getAttribute("operazioniSelezionate");
+    if (operazioniSelezionate == null) {
+      operazioniSelezionate = new ArrayList();
+    }
 
+    // recupero dalla sessione la lista degli operatori selezionati con decisione sull'operando 
+    // se non esite, la creo
+    ArrayList latoOperazioni = (ArrayList) sessione.getAttribute("latoOperazioni");
+    if (latoOperazioni == null) {
+      latoOperazioni = new ArrayList();
+    }
+    
+    // recupero dalla sessione la lista dei colori relativi agli operatori selezionati
+    // se non esite, la creo
+    ArrayList colori = (ArrayList) sessione.getAttribute("colori");
+    if (colori == null) {
+      colori = new ArrayList();
+    }
+    
+
+    ArrayList f1 = (ArrayList) sessione.getAttribute("f1");
+
+    if (f1 == null) {
+      f1 = new ArrayList();
+    }
+    
+    ArrayList f2 = (ArrayList) sessione.getAttribute("f2");
+    if (f2 == null) {
+      f2 = new ArrayList();
+    }
+
+    
+    response.setContentType("text/html");
+    response.getWriter().append("[");
+    for (int i = 0; i < operazioniSelezionate.size(); i++) {
+      int x = i * 2;
+      int j =  x + 1;
+      if (i != operazioniSelezionate.size() - 1) {
+        response.getWriter().append("{\"op\":\"" + operazioniSelezionate.get(i) 
+                + "\", \"scelta\": \"" + latoOperazioni.get(i) + "\", \"colore\": \""
+                + colori.get(i) + "\", \"f1N\": \"" + f1.get(x) + "\",  \"f1T\": \""
+                + f1.get(j) + "\", \"f2N\": \"" + f2.get(x) + "\", \"f2T\": \""
+                + f2.get(j) + "\"},");
+      } else {
+        response.getWriter().append("{\"op\":\"" + operazioniSelezionate.get(i) 
+                + "\", \"scelta\": \"" + latoOperazioni.get(i) + "\", \"colore\": \""
+                + colori.get(i) + "\", \"f1N\": \"" + f1.get(x) + "\",  \"f1T\": \""
+                + f1.get(j) + "\", \"f2N\": \"" + f2.get(x) + "\", \"f2T\": \""
+                + f2.get(j) + "\"}");
+      }
+    }
+    response.getWriter().append("]");
     sessione.removeAttribute("operazioniSelezionate");
     sessione.removeAttribute("latoOperazioni");
     sessione.removeAttribute("colori");
